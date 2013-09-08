882d1d9ef8c09766f7aa0fe9519fa150294f9689
Christophe <christophe.pele@gmail.com>
2013-09-08 20:31:37 +0200

    Deploy as executable jar To use wtt as a shell script, a jar file had to be created using maven, containing all dependencies.

--- wtt/src/main/java/com/github/geekarist/wtt/App.java
+++ wtt/src/main/java/com/github/geekarist/wtt/App.java
@@ -3,12 +3,16 @@ package com.github.geekarist.wtt;
 import java.io.File;
 
 public class App {
-    public static void main(String[] args) {
-        Movie movie = Movie.get("http://www.allocine.fr/film/fichefilm_gen_cfilm=26251.html");
-        System.out.println(movie.getTitle() + ":");
-        System.out.println("- Original title: " + movie.getOriginalTitle());
-        System.out.println("- Year of release: " + movie.getYear());
-        File torrent = Torrent.find(movie.getOriginalTitle() + " " + movie.getYear());
-        System.out.println(String.format("Torrent saved to [%s]", torrent.getPath()));
-    }
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Usage: wtt <url>");
+			System.exit(1);
+		}
+		Movie movie = Movie.get(args[0]);
+		System.out.println(movie.getTitle() + ":");
+		System.out.println("- Original title: " + movie.getOriginalTitle());
+		System.out.println("- Year of release: " + movie.getYear());
+		File torrent = Torrent.find(movie.getOriginalTitle() + " " + movie.getYear());
+		System.out.println(String.format("Torrent saved to [%s]", torrent.getPath()));
+	}
 }
