0e4b45ad93624fa885cad5d21711f61972f8e2c7
Christophe <christophe.pele@gmail.com>
2013-09-21 17:25:06 +0200

    Add -r option to rename torrent file after title Use JCommander to parse command line arguments. Fixes #1.

--- wtt/src/main/java/com/github/geekarist/wtt/App.java
+++ wtt/src/main/java/com/github/geekarist/wtt/App.java
@@ -1,18 +1,38 @@
 package com.github.geekarist.wtt;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
+    private static class Parameters {
+        @Parameter(names = {"-r", "--rename"}, description = "Rename torrent file after file name")
+        private boolean rename = false;
+
+        @Parameter
+        private List<String> arguments = new ArrayList<>();
+    }
+
 	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.out.println("Usage: wtt <url>");
-			System.exit(1);
-		}
-		Movie movie = Movie.get(args[0]);
+        Parameters parameters = new Parameters();
+        new JCommander(parameters, args);
+        if (parameters.arguments.size() == 0) {
+            System.out.println("Usage: wtt [-r|--rename] <url>");
+            System.exit(1);
+        }
+		Movie movie = Movie.get(parameters.arguments.get(0));
 		System.out.println(movie.getTitle() + ":");
 		System.out.println("- Original title: " + movie.getOriginalTitle());
 		System.out.println("- Year of release: " + movie.getYear());
 		File torrent = Torrent.find(movie.getOriginalTitle() + " " + movie.getYear());
 		System.out.println(String.format("Torrent saved to [%s]", torrent.getPath()));
+        if (parameters.rename) {
+            String newTorrentPath = String.format("%s-%s.torrent", movie.getOriginalTitle().toLowerCase(), movie.getYear());
+            torrent.renameTo(new File(newTorrentPath));
+            System.out.println(String.format("And renamed to [%s]", newTorrentPath));
+        }
 	}
 }
--- wtt/src/test/java/com/github/geekarist/wtt/AppTest.java
+++ wtt/src/test/java/com/github/geekarist/wtt/AppTest.java
@@ -1,38 +1,22 @@
 package com.github.geekarist.wtt;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import static org.fest.assertions.Assertions.assertThat;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+    @Test
+    public void shouldRenameTorrentFileIfOptionGiven() {
+        // GIVEN
+        new File("oblivion-2013.torrent").delete();
+        // WHEN
+        App.main((String[]) Arrays.asList("-r", "http://www.allocine.fr/film/fichefilm_gen_cfilm=27405.html").toArray());
+        // THEN
+        assertThat(new File("oblivion-2013.torrent")).exists();
     }
 }
